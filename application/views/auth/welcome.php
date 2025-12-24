9a8cdaef49f0f4bac3593022a9b2b98279245dcf
Viru0804 <dsaviru6@gmail.com>
2025-12-24 22:37:21 +0530

    partner hospital

--- application/views/auth/welcome.php
+++ application/views/auth/welcome.php
@@ -121,7 +121,7 @@
               <span class="card-visual">
                 <img src="<?= base_url('assets/img/manoday.png'); ?>" alt="Manoday logo">
                 <span class="card-text">
-                  <span class="card-title">MANODAY</span>
+                  <span class="card-title">Manoday</span>
                   <span class="card-sub">Mind Care Clinic</span>
                 </span>
                 <span class="card-radio" aria-hidden="true"></span>
